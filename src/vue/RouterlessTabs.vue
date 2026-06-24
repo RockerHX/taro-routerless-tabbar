ad84ab74cdfe1c7b3db9001fceae6698c10ff75f
RockerHX <rockerhx@gmail.com>
2026-06-24 22:04:23 +0800

    feat: 新增 RouterlessTabs 高阶组件

---
+++ src/vue/RouterlessTabs.vue
@@ -0,0 +1,100 @@
+<template>
+  <view class="routerless-tabs">
+    <RouterlessTabPaneHost
+      :items="tabs"
+      :active="tabsState.activeKey.value"
+      :visited="tabsState.visitedKeys.value"
+      :host-class="hostClass"
+      :pane-class="paneClass"
+      :hidden-class="hiddenClass"
+    >
+      <template #pane="{ pane, active }">
+        <slot name="pane" :pane="pane" :active="active">
+          <component :is="pane.component" embedded :active="active" />
+        </slot>
+      </template>
+    </RouterlessTabPaneHost>
+
+    <RouterlessTabBar
+      :active="tabsState.activeKey.value"
+      :items="tabs"
+      :refreshing="refreshing"
+      :refresh-icon="refreshIcon"
+      @change="handleChange"
+      @retap="handleRetap"
+    >
+      <template v-if="$slots.item" #item="slotProps">
+        <slot name="item" v-bind="slotProps" />
+      </template>
+    </RouterlessTabBar>
+  </view>
+</template>
+
+<script setup lang="ts">
+import type { Component, PropType } from 'vue'
+
+import type { RouterlessTabBarItem } from '../types.js'
+import RouterlessTabBar from './RouterlessTabBar.vue'
+import RouterlessTabPaneHost from './RouterlessTabPaneHost.vue'
+import { useRouterlessTabs } from './useRouterlessTabs.js'
+
+type ClassValue = string | string[] | Record<string, boolean>
+type RouterlessTabsItem = RouterlessTabBarItem & {
+  component: Component
+}
+
+const props = defineProps({
+  tabs: {
+    type: Array as PropType<readonly RouterlessTabsItem[]>,
+    required: true,
+  },
+  defaultKey: {
+    type: String,
+    required: true,
+  },
+  initialKey: {
+    type: String,
+    default: '',
+  },
+  refreshing: {
+    type: String,
+    default: '',
+  },
+  refreshIcon: {
+    type: String,
+    default: '',
+  },
+  hostClass: {
+    type: [String, Array, Object] as PropType<ClassValue>,
+    default: '',
+  },
+  paneClass: {
+    type: [String, Array, Object] as PropType<ClassValue>,
+    default: '',
+  },
+  hiddenClass: {
+    type: [String, Array, Object] as PropType<ClassValue>,
+    default: '',
+  },
+})
+
+const emit = defineEmits<{
+  change: [key: string]
+  retap: [key: string]
+}>()
+
+const tabsState = useRouterlessTabs({
+  tabs: props.tabs,
+  defaultKey: props.defaultKey,
+  initialKey: props.initialKey || undefined,
+})
+
+function handleChange(key: string) {
+  tabsState.activateTab(key)
+  emit('change', key)
+}
+
+function handleRetap(key: string) {
+  emit('retap', key)
+}
+</script>
